399adaf94cac74665462750b56356c49759f1d49
cloe <cromsal087@g.educaand.es>
2025-02-03 23:48:26 +0100

    no detecta css

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,28 @@
 {
-'name': 'Evaluación de desempeño',
-'version': '1.0',
-'summary': 'Módulo para gestionar el desempeño de cada empleado',
-'category': 'Productivity',
-'author': 'Cloe Romero Salguero',
-'website': 'https://tuweb.com',
-'license': 'LGPL-3',
-'depends': ['base', 'mail'],
-'icon': '/evaluacion_desempeno/static/description/iconodesempeno.png',
-'data': [
-'views/evaluacion_desempeno_views.xml',
-'security/ir.model.access.csv',
-],
-'application': True,
-'installable': True,
-'auto_install': False,
-'description': """
-Módulo de Odoo para la gestión del desempeño de cada empleado
-"""
+    'name': 'Evaluación de desempeño',
+    'version': '1.0',
+    'summary': 'Módulo para gestionar el desempeño de cada empleado',
+    'category': 'Productivity',
+    'author': 'Cloe Romero Salguero',
+    'website': 'https://tuweb.com',
+    'license': 'LGPL-3',
+    'depends': ['base', 'mail'],
+    'icon': '/evaluacion_desempeno/static/description/iconodesempeno.png',
+    'data': [
+        'views/evaluacion_desempeno_views.xml',
+        'security/ir.model.access.csv',
+    ],
+
+    
+    'assets': {
+        'web.assets_backend': [
+            '/evaluacion_desempeno/static/src/css/styles.css',
+        ],
+    },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'description': """
+    Módulo de Odoo para la gestión del desempeño de cada empleado
+    """
 }
